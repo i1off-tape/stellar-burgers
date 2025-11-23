3896fa062e7ce42d4e0a7b407a6d51593928ca35
Ivanov <walkodav.13@gmail.com>
2025-11-23 04:41:04 +0300

    -add modal system ( without api info )

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,145 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate,
+  Navigate
+} from 'react-router-dom';
+import { Modal } from '@components';
+import { IngredientDetails, OrderInfo } from '@components';
 
 import { AppHeader } from '@components';
 
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const isAuthenticated = true; // TODO: Сделать авторизацию
+
+  if (!isAuthenticated) {
+    return <Navigate to='/login' replace />;
+  }
+
+  return <>{children}</>;
+};
+
+//Модалка
+const ModalSwitch = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const background = location.state?.background;
+
+  const handleClose = () => {
+    navigate(-1);
+  };
+
+  return (
+    <>
+      <Routes location={background || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/login'
+          element={
+            <ProtectedRoute>
+              <Login />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <ProtectedRoute>
+              <Register />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <ProtectedRoute>
+              <ForgotPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <ProtectedRoute>
+              <ResetPassword />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/profile/orders'
+          element={
+            <ProtectedRoute>
+              <ProfileOrders />
+            </ProtectedRoute>
+          }
+        />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='' onClose={handleClose}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='Детали ингредиента' onClose={handleClose}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <ProtectedRoute>
+                <Modal title='' onClose={handleClose}>
+                  <OrderInfo />
+                </Modal>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      )}
+    </>
+  );
+};
+
 const App = () => (
   <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
+    <Router>
+      <AppHeader />
+      <ModalSwitch />
+    </Router>
   </div>
 );
 
