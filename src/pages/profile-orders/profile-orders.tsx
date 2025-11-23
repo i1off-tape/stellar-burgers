6023d9dce8c6b4b3cbcb2a7e99b27d1a444ef138
Vadim Ivanov <jhoncrendy18@gmail.com>
2025-11-23 15:22:27 +0300

    доп. проверка

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -4,14 +4,18 @@ import { FC, useEffect } from 'react';
 import { useSelector, useDispatch } from '../../services/store';
 import { fetchProfileOrders } from '../../services/thunks/feedsThunk';
 import { feedsSelectors } from '../../services/slices/feedsSlice';
+import { userSelectors } from '../../services/slices/userSlice';
 
 export const ProfileOrders: FC = () => {
   const orders = useSelector(feedsSelectors.feedOrdersSelect);
+  const user = useSelector(userSelectors.userSelect);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchProfileOrders());
-  }, [dispatch]);
+    if (user) {
+      dispatch(fetchProfileOrders());
+    }
+  }, [dispatch, user]);
 
   return <ProfileOrdersUI orders={orders} />;
 };
