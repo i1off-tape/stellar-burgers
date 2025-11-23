e96a6f72393b196ca2d4f78cfead5e86509b7b9b
Vadim Ivanov <jhoncrendy18@gmail.com>
2025-11-23 14:58:55 +0300

    в профиле показана история заказов

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,17 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useSelector, useDispatch } from '../../services/store';
+import { fetchProfileOrders } from '../../services/thunks/feedsThunk';
+import { feedsSelectors } from '../../services/slices/feedsSlice';
 
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const orders = useSelector(feedsSelectors.feedOrdersSelect);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProfileOrders());
+  }, [dispatch]);
 
   return <ProfileOrdersUI orders={orders} />;
 };
